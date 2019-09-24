30d24df35037c9b7a83717386f0402cedac0d4f9
gabus1989 <55726405+gabus1989@users.noreply.github.com>
2019-09-24 16:37:17 +0700

    Update newhead.js

--- newhead.js
+++ newhead.js
@@ -1,4 +1,4 @@
 var newhead = document.getElementById("newhead");
 var body = document.body;
 body.insertBefore(newhead, body.childNodes[0]);
-document.getElementById("content").childNodes[0].style.display = "none";
+document.getElementById("content").children[0].style.display = "none";
