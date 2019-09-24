453f86d281a4a9fd1bf24caf2bdc882a29c2a2c9
gabus1989 <55726405+gabus1989@users.noreply.github.com>
2019-09-24 17:10:21 +0700

    Update newhead.js

--- newhead.js
+++ newhead.js
@@ -1,7 +1,8 @@
-var newhead = document.getElementById("newhead");
+var newhead = document.getElementById("navnewhead");
 var body = document.body;
 body.insertBefore(newhead, body.childNodes[0]);
 document.getElementById("content").children[0].style.display = "none";
+
 document.getElementById("menubutton").addEventListener("click", function(){
   document.getElementById("newhead").innerHTML = "Hello World";
 });
