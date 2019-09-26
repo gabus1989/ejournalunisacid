dc33bc4cfc8083e42e315623351c215c3959ef58
gabus1989 <55726405+gabus1989@users.noreply.github.com>
2019-09-26 07:58:47 +0700

    Update newhead.js

--- newhead.js
+++ newhead.js
@@ -5,4 +5,5 @@ document.getElementById("content").children[0].style.display = "none";
 
 document.getElementById("menubutton").addEventListener("click", function(){
   document.getElementById("ulnewnav").style.display = "none";
+  document.getElementById("ulnewnav").style.backgroundColor = "#21a7ac";  
 });
