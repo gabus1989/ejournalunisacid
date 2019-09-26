ffc8366321a71921c0a7d37db8f471309c342dd1
gabus1989 <55726405+gabus1989@users.noreply.github.com>
2019-09-26 16:54:14 +0700

    Update newhead.js

--- newhead.js
+++ newhead.js
@@ -3,8 +3,8 @@ var body = document.body;
 body.insertBefore(newhead, body.childNodes[0]);
 document.getElementById("content").children[0].style.display = "none";
 
-var element = document.getElementById("divmenupanel"),
-    style = window.getComputedStyle(element),
+var element = document.getElementById("divmenupanel");
+    style = window.getComputedStyle(element);
     top = style.getPropertyValue("display");
 
 alert(top);
