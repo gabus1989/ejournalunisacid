c2d69b2d30715f081d9a8cef046bb9a02df468f4
gabus1989 <55726405+gabus1989@users.noreply.github.com>
2019-09-26 17:06:59 +0700

    Update newhead.js

--- newhead.js
+++ newhead.js
@@ -3,9 +3,11 @@ var body = document.body;
 body.insertBefore(newhead, body.childNodes[0]);
 document.getElementById("content").children[0].style.display = "none";
 
+/*
 var elem = document.getElementById("divmenupanel");
 var theCSSprop = window.getComputedStyle(elem, null).getPropertyValue("display");
 alert(theCSSprop);
+*/
 
 document.getElementById("menubutton").addEventListener("click", function(){
   var divmenupanel = document.getElementById("divmenupanel");
