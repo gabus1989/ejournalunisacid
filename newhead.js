86e7962daf23ff10170dde92318a76380547766c
gabus1989 <55726405+gabus1989@users.noreply.github.com>
2019-09-27 14:56:36 +0700

    Update newhead.js

--- newhead.js
+++ newhead.js
@@ -1,6 +1,9 @@
+//pindahkan navnewhead
 var newhead = document.getElementById("navnewhead");
 var body = document.body;
 body.insertBefore(newhead, body.childNodes[0]);
+
+// sembunyikan judul
 document.getElementById("content").children[0].style.display = "none";
 
 /*
@@ -9,16 +12,17 @@ var theCSSprop = window.getComputedStyle(elem, null).getPropertyValue("display")
 alert(theCSSprop);
 */
 
+//buka tutup menu
 document.getElementById("menubutton").addEventListener("click", function(){
   var divmenupanel = document.getElementById("divmenupanel");
   divmenupanel.style.display = "block";
 });
-
 document.getElementById("abackbutton").addEventListener("click", function(){
   var divmenupanel = document.getElementById("divmenupanel");
     divmenupanel.style.display = "none";
 });
 
-var x = document.getElementById("sidebarUser").tagName;
-var res = String(x);
-alert(res);
+
+var sidebarUser = document.getElementById("sidebarUser");
+var divmenupanel = document.getElementById("divmenupanel");
+divmenupanel.insertBefore(sidebarUser, divmenupanel.childNodes[0]);
